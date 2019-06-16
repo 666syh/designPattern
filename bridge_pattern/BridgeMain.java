4979daa8393cb1663a463dcfe034f05781fc8e2c
1317578865 <1317578865@qq.com>
2019-06-16 22:06:42 +0800

    一个简单的程序（vscode java编译出bug，重新配置用户设置）

---
+++ bridge_pattern/BridgeMain.java
@@ -0,0 +1,7 @@
+package bridge_pattern;
+public class BridgeMain{
+    public static void main(String[] args) {
+        HandsetNGame game = new HandsetNGame();
+        game.run();
+    }
+}
---
+++ bridge_pattern/HandsetNGame.java
@@ -0,0 +1,6 @@
+package bridge_pattern;
+public class HandsetNGame{
+    public void run(){
+        System.out.println("运行N品牌手机游戏");
+    }
+}
