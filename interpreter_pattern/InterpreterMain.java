b8ba4cdc2602442bebbf5a1d9bd207326864a82b
1317578865 <1317578865@qq.com>
2019-06-24 19:34:23 +0800

    横向增加扩展

--- interpreter_pattern/InterpreterMain.java
+++ interpreter_pattern/InterpreterMain.java
@@ -3,14 +3,18 @@ public class InterpreterMain{
     public static void main(String[] args) {
         PlayContext context = new PlayContext();
         System.out.println("上海滩");
-        context.setText("O 2 E 0.5 G 0.5 A 3 E 0.5 G 0.5 D 3 E 0.5 G 0.5 A 0.5 O 3 C 1 O 2 A 0.5 G 1 C 0.5 E 0.5 D 3 ");
+        context.setText("T 500 O 2 E 0.5 G 0.5 A 3 E 0.5 G 0.5 D 3 E 0.5 G 0.5 A 0.5 O 3 C 1 O 2 A 0.5 G 1 C 0.5 E 0.5 D 3 ");
         Expression expression = null;
         try {
+            //由于增加扩展对客户端也有影响，因此应把下述代码应用简单工厂模式，并利用反射原理实现解耦
             while(context.getText().length() > 0){
                 String str = context.getText().substring(0, 1);
                 switch (str) {
                     case "O":
                         expression = new Scale();
+                        break;
+                    case "T":
+                        expression = new Speed();
                         break;
                     case "C":
                     case "D":
---
+++ interpreter_pattern/Speed.java
@@ -0,0 +1,19 @@
+package interpreter_pattern;
+public class Speed extends Expression{
+
+    @Override
+    public void excute(String key, double value) {
+        String speed = "";
+        if(value < 500){
+            speed = "快速";
+        }
+        else if(value >= 1000){
+            speed = "慢速";
+        }
+        else{
+            speed = "中速";
+        }
+        System.out.print(speed+" ");
+    }
+    
+}
